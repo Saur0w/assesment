16f5d5653dfb88b5b252242b5eb3a4103669195d
saur0w <sthapliyal085@gmail.com>
2026-02-18 13:20:04 +0530

    first commit

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styles from './style.module.scss';
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+export default function Landing() {
+    return (
+        <section className={styles.landing}>
+
+        </section>
+    )
+}
---
+++ src/ui/Text/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Text() {
+    return (
+        <div>
+
+        </div>
+    )
+}
